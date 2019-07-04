135ef1255c7f2395dd6085ce9ce7579b2b0d0a8c
Shota Arakaki <syotaa1@gmail.com>
2019-07-04 20:13:25 +0900

    fix

--- src/Text/TinySegmenter.hs
+++ src/Text/TinySegmenter.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE BangPatterns #-}
 module Text.TinySegmenter where
 
+import           Control.DeepSeq
 import           Control.Monad.Trans.State
 import qualified Data.HashSet                  as S
 import           GHC.Prim
@@ -27,19 +28,27 @@ ct2i c = case c of
 
 
 getCTypes :: Char -> Int#
-getCTypes c | S.member c m = ct2i TM
-            | S.member c h = ct2i TH
-            | 'ぁ' <= c && c <= 'ん' = ct2i TI
-            | 'ァ' <= c && c <= 'ヴ'
-              || 'ｱ' <= c && c <= 'ﾝ'
-              || S.member c ksub = ct2i TK
-            | 'a' <= c && c <= 'z'
-              || 'A' <= c && c <= 'Z'
-              || 'ａ' <= c && c <= 'ｚ'
-              || 'Ａ' <= c && c <= 'Ｚ' = ct2i TA
-            | '0' <= c && c <= '9'
-              || '０' <= c && c <= '９' = ct2i TN
-  where
-    !m = S.fromList "一二三四五六七八九十百千万億兆"
-    !h = S.fromList "々〆ヵヶ"
-    !ksub = S.fromList "ーｰ\xff9e"
+getCTypes c
+  | S.member c m
+  = ct2i TM
+  | '一' <= c && c <= '龠'
+    || S.member c h
+  = ct2i TH
+  | 'ぁ' <= c && c <= 'ん'
+  = ct2i TI
+  | 'ァ' <= c && c <= 'ヴ'
+    || 'ｱ' <= c && c <= 'ﾝ'
+    || S.member c ksub
+  = ct2i TK
+  | 'a' <= c && c <= 'z'
+    || 'A' <= c && c <= 'Z'
+    || 'ａ' <= c && c <= 'ｚ'
+    || 'Ａ' <= c && c <= 'Ｚ'
+  = ct2i TA
+  | '0' <= c && c <= '9'
+    || '０' <= c && c <= '９'
+  = ct2i TN
+ where
+  !m    = let m = S.fromList "一二三四五六七八九十百千万億兆" in deepseq m m
+  !h    = let h = S.fromList "々〆ヵヶ" in deepseq h h
+  !ksub = let ksub = S.fromList "ーｰ\xff9e" in deepseq ksub ksub
