bbce79ae0622070d9c372e0c2858c83d40b83cc1
Shota Arakaki <syotaa1@gmail.com>
2019-07-05 10:44:57 +0900

    fix

--- src/Text/TinySegmenter.hs
+++ src/Text/TinySegmenter.hs
@@ -8,89 +8,103 @@
 module Text.TinySegmenter where
 
 import           Control.Monad.Trans.State
-import qualified Data.HashMap.Strict           as M
+import           Data.Char
 import qualified Data.HashSet                  as S
 import qualified Data.Text                     as T
 import qualified Data.Text.Array               as TA
+import           Data.Word
 import           GHC.Exts
+
+-- markers, which values are outside of unicode code point
+b1,b2,b3,e1,e2,e3 :: Int#
+b1 = 0x110001#
+b2 = 0x110002#
+b3 = 0x110003#
+e1 = 0x110004#
+e2 = 0x110005#
+e3 = 0x110006#
 
 data Marker = U | O | B
 data CTypes = TM | TH | TI | TK | TA | TN | TO
 
+mk2i :: Marker -> Word8
 mk2i m = case m of
-  U -> fromEnum 'U'
-  O -> fromEnum 'O'
-  B -> fromEnum 'B'
+  U -> $([| toEnum $ fromEnum 'U' |])
+  O -> $([| toEnum $ fromEnum 'O' |])
+  B -> $([| toEnum $ fromEnum 'B' |])
 {-# INLINE mk2i #-}
 
+ct2i :: CTypes -> Word8
 ct2i c = case c of
-  TM -> fromEnum 'M'
-  TH -> fromEnum 'H'
-  TI -> fromEnum 'I'
-  TK -> fromEnum 'K'
-  TA -> fromEnum 'A'
-  TN -> fromEnum 'N'
-  TO -> fromEnum 'O'
+  TM -> $([| toEnum $ fromEnum 'M' |])
+  TH -> $([| toEnum $ fromEnum 'H' |])
+  TI -> $([| toEnum $ fromEnum 'I' |])
+  TK -> $([| toEnum $ fromEnum 'K' |])
+  TA -> $([| toEnum $ fromEnum 'A' |])
+  TN -> $([| toEnum $ fromEnum 'N' |])
+  TO -> $([| toEnum $ fromEnum 'O' |])
 {-# INLINE ct2i #-}
 
-getCTypes :: Char -> Int
+getCTypes :: Int -> Word8
 getCTypes c
   | S.member c m
   = ct2i TM
-  | ('一' <= c && c <= '龠') || S.member c h
+  | (ord '一' <= c && c <= ord '龠') || S.member c h
   = ct2i TH
-  | 'ぁ' <= c && c <= 'ん'
+  | ord 'ぁ' <= c && c <= ord 'ん'
   = ct2i TI
-  | ('ァ' <= c && c <= 'ヴ') || ('ｱ' <= c && c <= 'ﾝ') || S.member c ksub
+  | (ord 'ァ' <= c && c <= ord 'ヴ')
+    || (ord 'ｱ' <= c && c <= ord 'ﾝ') || S.member c ksub
   = ct2i TK
-  | ('a' <= c && c <= 'z')
-    || ('A' <= c && c <= 'Z')
-    || ('ａ' <= c && c <= 'ｚ')
-    || ('Ａ' <= c && c <= 'Ｚ')
+  | (ord 'a' <= c && c <= ord 'z')
+    || (ord 'A' <= c && c <= ord 'Z')
+    || (ord 'ａ' <= c && c <= ord 'ｚ')
+    || (ord 'Ａ' <= c && c <= ord 'Ｚ')
   = ct2i TA
-  | ('0' <= c && c <= '9') || ('０' <= c && c <= '９')
+  | (ord '0' <= c && c <= ord '9') || (ord '０' <= c && c <= ord '９')
   = ct2i TN
   | otherwise
   = ct2i TO
   where
-    m    = $([| S.fromList "一二三四五六七八九十百千万億兆" |])
-    h    = $([| S.fromList "々〆ヵヶ" |])
-    ksub = $([| S.fromList "ーｰ\xff9e" |])
+    m    = $([| S.fromList $ fmap ord "一二三四五六七八九十百千万億兆" |])
+    h    = $([| S.fromList $ fmap ord "々〆ヵヶ" |])
+    ksub = $([| S.fromList $ fmap ord "ーｰ\xff9e" |])
 {-# INLINABLE getCTypes #-}
 
-takeThree :: T.Text -> (# (# Char | () #), (# Char | () #), (# Char | () #), T.Text #)
+takeThree :: T.Text -> (# Int#, Int#, Int#, T.Text #)
 takeThree text = case T.uncons text of
-  Nothing -> (# (# | () #), (# | () #), (# | () #), text #)
-  Just (ca, ra) -> case T.uncons ra of
-    Nothing -> (# (# ca | #), (# | () #), (# | () #), ra #)
-    Just (cb, rb) -> case T.uncons rb of
-      Nothing -> (# (# ca | #), (# cb | #), (# | () #), rb #)
-      Just (cc, rc) ->(# (# ca | #), (# cb | #), (# cc | #), rc #)
+  Nothing -> (# e1, e2, e3, text #)
+  Just (C# ca, ra) -> case T.uncons ra of
+    Nothing -> (# ord# ca, e1, e2, ra #)
+    Just (C# cb, rb) -> case T.uncons rb of
+      Nothing -> (# ord# ca, ord# cb, e1, rb #)
+      Just (C# cc, rc) ->(# ord# ca, ord# cb, ord# cc, rc #)
 {-# INLINE takeThree #-}
 
-mapCType :: (# Char | () #) -> Int
-mapCType (# a | #) = getCTypes a
-mapCType (# | () #) = mk2i O
-{-# INLINE mapCType #-}
+takeOne :: T.Text -> (# (# Int#, T.Text #) | () #)
+takeOne text = case T.uncons text of
+  Nothing -> (# | () #)
+  Just (C# c, r) -> (# (# ord# c, r #) | #)
+{-# INLINE takeOne #-}
 
-data TokenizeState = TS { remain :: !T.Text
+data TokenizeState = TS { remain :: {-# UNPACK #-} !T.Text
                         -- , word :: LT.Builder
                         , score :: {-# UNPACK #-} !Int
-                        , p1 :: {-# UNPACK #-} !Int
-                        , p2 :: {-# UNPACK #-} !Int
-                        , p3 :: {-# UNPACK #-} !Int
-                        , w1 :: !(# Char | () #)
-                        , w2 :: !(# Char | () #)
-                        , w3 :: !(# Char | () #)
-                        , w4 :: !(# Char | () #)
-                        , w5 :: !(# Char | () #)
-                        , w6 :: !(# Char | () #)
-                        , c1 :: {-# UNPACK #-} !Int
-                        , c2 :: {-# UNPACK #-} !Int
-                        , c3 :: {-# UNPACK #-} !Int
-                        , c4 :: {-# UNPACK #-} !Int
-                        , c5 :: {-# UNPACK #-} !Int
-                        , c6 :: {-# UNPACK #-} !Int
+                        , p1 :: {-# UNPACK #-} !Word8
+                        , p2 :: {-# UNPACK #-} !Word8
+                        , p3 :: {-# UNPACK #-} !Word8
+                        , w1 :: {-# UNPACK #-} !Int#
+                        , w2 :: {-# UNPACK #-} !Int#
+                        , w3 :: {-# UNPACK #-} !Int#
+                        , w4 :: {-# UNPACK #-} !Int#
+                        , w5 :: {-# UNPACK #-} !Int#
+                        , w6 :: {-# UNPACK #-} !Int#
+                        , c1 :: {-# UNPACK #-} !Word8
+                        , c2 :: {-# UNPACK #-} !Word8
+                        , c3 :: {-# UNPACK #-} !Word8
+                        , c4 :: {-# UNPACK #-} !Word8
+                        , c5 :: {-# UNPACK #-} !Word8
+                        , c6 :: {-# UNPACK #-} !Word8
                         }
 
 makeInitialState :: T.Text -> TokenizeState
@@ -101,18 +115,18 @@ makeInitialState text =
      , p1 = mk2i U
      , p2 = mk2i U
      , p3 = mk2i U
-     , w1 = (# | () #)
-     , w2 = (# | () #)
-     , w3 = (# | () #)
+     , w1 = b1
+     , w2 = b2
+     , w3 = b3
      , w4 = a
      , w5 = b
      , w6 = c
      , c1 = mk2i O
      , c2 = mk2i O
      , c3 = mk2i O
-     , c4 = mapCType a
-     , c5 = mapCType b
-     , c6 = mapCType c
+     , c4 = getCTypes a
+     , c5 = getCTypes b
+     , c6 = getCTypes c
      }
   where
     bias = -332
